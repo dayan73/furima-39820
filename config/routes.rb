11faaabeb04f2fdd826f3f8ed4ecf8bd4c4a3003
dayan73 <nmazaki@hotmail.com>
2024-01-29 02:23:01 +0900

    出品を降順に指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   delete 'logout', to: 'devise/sessions#destroy', as: 'logout'
-  
+
   get 'items/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/items/new', to: 'items#new', as: 'new_item'
   # Defines the root path route ("/")
-    root 'items#index'
-resources :items, only: [:new, :create]
+  root 'items#index'
 
+  resources :items, only: [:new, :create, :index]
+
+  resources :items do
+    collection do
+      get 'index', to: 'items#index', as: 'public_index'
+    end
+  end
 end
+
