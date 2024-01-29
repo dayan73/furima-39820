cd3e9e4f182c5e5ff1748bf41c56823fc7e57e13
dayan73 <nmazaki@hotmail.com>
2024-01-29 14:36:30 +0900

    商品一覧表示機能　修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,11 @@ Rails.application.routes.draw do
 
   get 'items/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/items/new', to: 'items#new', as: 'new_item'
+  
   # Defines the root path route ("/")
   root 'items#index'
 
   resources :items, only: [:new, :create, :index]
 
-  resources :items do
-    collection do
-      get 'index', to: 'items#index', as: 'public_index'
-    end
   end
-end
 
