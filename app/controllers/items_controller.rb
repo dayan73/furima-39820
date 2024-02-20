674dc0d88ceb9f05ec465d37d7ef367c749bbddb
dayan73 <nmazaki@hotmail.com>
2024-02-20 11:05:02 +0900

    controller修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
-  before_action :redirect_sold_out, only: [:show]
   
-
   def index
     @items = Item.order(created_at: :desc)
   end
