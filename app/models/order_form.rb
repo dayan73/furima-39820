06261c1d018c6efd08030b6b31006515f1dc7818
dayan73 <nmazaki@hotmail.com>
2024-02-20 11:40:46 +0900

    バリデーション修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -4,11 +4,10 @@ class OrderForm
 
   with_options presence: true do
     validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :region_id, numericality: { other_than: 0, message: "can't be blank"}
+    validates :region_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :house_number
-    validates :phone_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+    validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
     validates :user_id
     validates :item_id
     validates :token
