de5029139d13ded9fc38553632ff1e96f2f67e00
dayan73 <nmazaki@hotmail.com>
2024-02-20 14:17:33 +0900

    都道府県のバリデーション修正（2）

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -4,7 +4,7 @@ class OrderForm
 
   with_options presence: true do
     validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :region_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :house_number
     validates :phone_number, presence: {message: "can't be blank"}, format: { with: /\A\d{10,11}\z/, message: 'is invalid', allow_blank: true }
