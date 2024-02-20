3baf188ed3014fff81f2f76a01b107e263fa27dc
dayan73 <nmazaki@hotmail.com>
2024-02-20 11:55:03 +0900

    電話番号のバリデーション修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -7,7 +7,7 @@ class OrderForm
     validates :region_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :house_number
-    validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+    validates :phone_number, presence: {message: "can't be blank"}, format: { with: /\A\d{10,11}\z/, message: 'is invalid', allow_blank: true }
     validates :user_id
     validates :item_id
     validates :token
